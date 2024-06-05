a47d90aa619833aeb2721d35008d3fed6349dd6d
Isidoro <marcelo.idantonio@senacsp.edu.br>
2024-06-05 20:04:08 -0300

    Insercao dos pacientes em suas devidas cidades

---
+++ Senaclin_massaDeDados.sql
@@ -0,0 +1,11 @@
+
+/* 2. Inserir 6 pacientes de forma implícita, sendo 3 de Santos, 1 de Guarujá e 2 de São Vicente; */
+
+INSERT INTO tb_pacientes (nome_paciente, cpf, tipo_logradouro, nomeLogradouro, numero, complemento, telefone_paciente, tipo_contato, cidade)
+VALUES 
+('Mirela', 12345678911, 'Rua', 'Capitão casca', 48, 'Casa 2', 13998761234, 'Celular', 'Guarujá'),
+('Carlos', 12345678912, 'Avenida', 'Castelo Blanco', 455,' ', 11996345872, 'Celular', 'Santos'),
+('Matias', 12345678913, 'Praça', 'Árvore da Sé', 9, 'Apto. 45', 1335789514, 'Telefone fixo', 'Santos'),
+('Josy', 12345678914, 'Rua', 'Monte Byakou', 55,' ', 13951357462, 'Celular', 'São Vicente'),
+('Abel', 12345678915, 'Avenida', 'Mascarenha de Moraes', 4688,' ', 13986451235, 'Celular', 'Santos'),
+('Lorrayne', 12345678916, 'Rua', 'Marquês Coutin', 66, 'Fundos', 1335469845, 'Telefone Fixo', 'São Vicente');
